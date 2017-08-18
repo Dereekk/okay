b2d6bd1af9fdefddea1dbf3fbf3ff9dd7ad33b73
Derk <irkedmore@gmail.com>
2017-08-17 22:29:37 -0600

    added

--- sw.js
+++ sw.js
@@ -76,3 +76,18 @@ self.addEventListener('fetch', function (e) {
         })
 	);
 });
+
+self.addEventListener('beforeinstallprompt', function(e) {
+
+  e.userChoice.then(function(choiceResult) {
+
+    console.log(choiceResult.outcome);
+
+    if(choiceResult.outcome == 'dismissed') {
+      console.log('User cancelled home screen install');
+    }
+    else {
+      console.log('User added to home screen');
+    }
+  });
+});
