6d4890516e902e58043c7825cf897e9902b67186
Derk <irkedmore@gmail.com>
2017-08-30 15:11:09 -0600

    Commit

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@ $.ajax({
                 $("#apod_img_id").attr("src", result.url);
             }
         $("#reqObject").text(url);
-        $("#returnObject").text(JSON.stringify(result, null, 4));
+        //$("#returnObject").text(JSON.stringify(result, null, 4));
         $("#apod_explaination").text(result.explanation);
         $("#apod_title").text(result.title);
     }
--- sw.js
+++ sw.js
@@ -1,46 +1,46 @@
 var staticCacheName = 'okay-static-v1';
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open(staticCacheName).then(function(cache){
-        return cache.addAll([
-            '/',
-            'index.html',
-            'css/style.css',
-            'js/app.js',
-            'manifest.json',
-            'sw.js',
-            'https://api.nasa.gov/planetary/apod?api_key=Ba3wAm9ImsmVvF8WxEs34fWeQkxeWAImYWFW0fWn'
-        ]);
+    caches.open(staticCacheName).then(function (cache) {
+      return cache.addAll([
+        '/',
+        'index.html',
+        'css/style.css',
+        'js/app.js',
+        'manifest.json',
+        'sw.js',
+        'https://api.nasa.gov/planetary/apod?api_key=Ba3wAm9ImsmVvF8WxEs34fWeQkxeWAImYWFW0fWn'
+      ]);
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
-    var requestUrl = new URL(event.request.url);
-    
-    if (requestUrl.origin === location.origin) {
-        if (requestUrl.pathname === '/') {
-            event.respondWith(caches.match('/skeleton'));
-            return
-        }
+self.addEventListener('fetch', function (event) {
+  var requestUrl = new URL(event.request.url);
+
+  if (requestUrl.origin === location.origin) {
+    if (requestUrl.pathname === '/') {
+      event.respondWith(caches.match('/skeleton'));
+      return
     }
-    
-    event.respondWith(
-    caches.match(event.request).then(function(response) {
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
       return response || fetch(event.request);
     })
   );
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
+    caches.keys().then(function (cacheNames) {
       return Promise.all(
-        cacheNames.filter(function(cacheName) {
+        cacheNames.filter(function (cacheName) {
           return cacheName.startsWith('okay-') &&
-                 cacheName != staticCacheName;
-        }).map(function(cacheName) {
+            cacheName != staticCacheName;
+        }).map(function (cacheName) {
           return caches.delete(cacheName);
         })
       );
@@ -48,8 +48,8 @@ self.addEventListener('activate', function(event) {
   );
 });
 
-self.addEventListener('message', function(event) {
-    if(event.data.action == 'skipWaiting') {
-        self.skipWaiting();
-    }
+self.addEventListener('message', function (event) {
+  if (event.data.action == 'skipWaiting') {
+    self.skipWaiting();
+  }
 });
