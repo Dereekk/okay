cca4aa89f4699637ec53a0e23dc02d6d9831435e
Derk <irkedmore@gmail.com>
2017-09-01 23:24:44 -0600

    :+1:

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 if (navigator.serviceWorker) {
     
-    navigator.serviceWorker.register('./sw.js', {scope: './'}).then(function (registration) {
+    navigator.serviceWorker.register('/sw.js', {scope: './'}).then(function (registration) {
         "use strict";
         console.log("Service Worker Registered", registration);
     })
