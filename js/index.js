5e93e91b12c62f72ad85e84ce05d24dfcb2d51d9
Derk <irkedmore@gmail.com>
2017-09-24 21:51:54 -0600

    okay

--- js/index.js
+++ js/index.js
@@ -1,10 +1,6 @@
-if (navigator.serviceWorker) {
-    
-      navigator.serviceWorker.register('./sw-v2.js', { scope: './' }).then(function(registration) {
-          console.log("Service Worker Registered");
-        })
-        .catch(function(err) {
-          console.log("Service Worker Failed to Register", err);
-        })
-    
-    }
+if(navigator.serviceWorker) {
+  navigator.serviceWorker.register('./sw_3.js')
+  .catch(function(err) {
+    console.error('Unable to register service worker.', err);
+  });
+}
